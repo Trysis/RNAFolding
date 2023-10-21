be01cb1dc7915f8e94543548233474d62d77e1f8
Roude JEAN MARIE <roude.etu@gmail.com>
2023-10-21 11:31:03 +0200

    save model

--- model.py
+++ model.py
@@ -20,9 +20,11 @@ def set_optimizer(model, optimizer, loss):
     return model
 
 
-def save_model(model, dirpath="", filename="model", ext="keras"):
+def save_model(model, dirpath="", dir_sep="/", filename="model", ext="keras"):
     if not aux.isdir(dirpath):
         raise Exception("Dirpath is invalid")
     # Save model with attributed extension
+    dirpath = aux.to_dirpath(dirpath, dir_sep=dir_sep)
     filename_ext = aux.replace_extension(filename, ext)
+    filepath = f"{dirpath}{filename_ext}"
     model.save(filepath)
